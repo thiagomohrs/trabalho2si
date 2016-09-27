d48c086d393f70e93ec8ffbc61a6b1067db704ce
Thiago Mohr da Silveira <thiagomohrs@gmail.com>
2016-09-27 17:04:26 -0300

    xx

---
+++ src/trabalho2si/Minimax.java
@@ -0,0 +1,48 @@
+package trabalho2si;
+
+public class Minimax {
+	int profundidade;
+
+	public int calcularMinimax() {
+		return 0;
+	}
+
+	public void podaMinimax() {
+
+	}
+
+	public void criarNodoFilho() {
+
+	}
+
+	public boolean verificaVitoriaPecaJogada() {
+		return false;
+	}
+
+	public int analisaColunaPecaJogada() {
+		int quantidadePecasSequencia = 0;
+		return quantidadePecasSequencia;
+	}
+
+	public int analisaLinhaPecaJogada() {
+		int quantidadePecasSequencia = 0;
+		return quantidadePecasSequencia;
+	}
+
+	public int analisaDiagonaisPecaJogada() {
+		int quantidadePecasSequencia = 0;
+
+		// Chama outros metodos pra analizar diagonais
+		return quantidadePecasSequencia;
+	}
+
+	public int analisaDiagonalEsquerdaInferior() {
+		int quantidadePecasSequencia = 0;
+		return quantidadePecasSequencia;
+	}
+
+	public int analisaDiagonalDireiraInferior() {
+		int quantidadePecasSequencia = 0;
+		return quantidadePecasSequencia;
+	}
+}
