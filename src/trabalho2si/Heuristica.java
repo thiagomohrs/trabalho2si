69cf60de9382386d5c71d8a4ae41f0232a622ca0
Thiago Mohr <thiagomohrs@gmail.com>
2016-10-19 18:31:19 -0200

    19/10

--- src/trabalho2si/Heuristica.java
+++ src/trabalho2si/Heuristica.java
@@ -1,5 +1,7 @@
 package trabalho2si;
 
+import java.util.ArrayList;
+
 public class Heuristica {
 
 	public int calculaHeuristicaEstado(Nodo nodo){
@@ -17,38 +19,91 @@ public class Heuristica {
 		return pontuacao;
 	}
 	
-	public int calculaHeuristica(Nodo nodo){
-		
-		return 0;
+	public int[] calculaHeuristica(Nodo nodo){
+		int peca;
+		int ArrayList listaSequenciasJogador = this.getSequencias(int[0][0] ,nodo, 1);
+		       	int pontComp = 0;
+		        int pontJog = 0;
+		        boolean jogador = false;
+		        int pontuacao[];
+				int[] temp = {0,0};
+		        //a cada jogada precisa calcular
+		        for (int i = 0; i < listaSequenciasJogador.; i++) {
+					if(jogador){
+						if (listaSequenciasJogador[i] == 4){
+							pontJog += 10 * 4;}
+						else{
+							if (listaSequenciasJogador[1] == 3){
+                        	pontJog += 10 * 3;
+                        	}
+						else{
+							if (listaSequenciasJogador[1] == 2){
+                        	pontJog += 10 * 2;
+                        	}
+						}
+					}
+				}
+				if(!jogador){
+					if (listaSequenciasJogador[1] == 4){
+						pontComp += 10 * 4;
+						}
+						else{
+						if (listaSequenciasJogador[1] == 3){
+						pontComp += 10 * 3;
+						}
+                       	else{
+                   		if (listaSequenciasJogador[1] == 2){
+           				pontComp += 10 * 2;
+                       	}
+					}
+				}
+			}
+		}
+		int[] heuristica = {pontJog, pontComp};
+		if (heuristica[0] != temp[0] || heuristica[1] != [1]) {
+			int[] temp = heuristica;
+		}
+		return heuristica;
 	}
 	
-	public int[] getSequencias(Nodo nodo){
-		int [] listaSequencias;
-		        listaSequencias.add(this.analisaColunaPecaTopo(nodo));
-		        listaSequencias.add(this.analisaLinhaPecaTopo(nodo));
-		        listaSequencias.add(this.analisaDiagDirPecaTopo(nodo));
-		        listaSequencias.add(this.analisaDiagEsqPecaTopo(nodo));
+	public ArrayList getSequencias(int[] ultimaPosicao, Nodo nodo, int jogador){
+		ArrayList<Integer> listaSequencias = new ArrayList<>();
+//		        listaSequencias.add(this.analisaColunaPecaTopo(nodo));
+//		        listaSequencias.add(this.analisaLinhaPecaTopo(nodo));
+//		        listaSequencias.add(this.analisaDiagDirPecaTopo(nodo));
+		        listaSequencias.add(this.analisaDiagEsqPecaTopo(ultimaPosicao,nodo, jogador));
 
 		        return listaSequencias;
 	}
 
-	private int analisaDiagEsqPecaTopo(Nodo nodo) {
-		// TODO Auto-generated method stub
-		return 0;
+	private int analisaDiagEsqPecaTopo(int[] ultimaPosicao, Nodo nodo, int jogador) {
+		int quantidadeSequencia = 1;
+		for (int linha = ultimaPosicao[0]; linha <= 0; linha--) {
+			for (int coluna = ultimaPosicao[1]; coluna <= 0; coluna--) {
+				if(linha >= 0 && linha<= 6 && coluna >= 0 && coluna <= 7){
+					if(nodo.estado[linha][coluna] == jogador){
+						quantidadeSequencia++;
+					} else{
+						return quantidadeSequencia;
+					}
+				}
+			}
+		}
+		return quantidadeSequencia;
 	}
 
-	private int analisaDiagDirPecaTopo(Nodo nodo) {
-		// TODO Auto-generated method stub
-		return 0;
+	private int[] analisaDiagDirPecaTopo(Nodo nodo) {
+		int[] sequencia = null;
+		return sequencia;
 	}
 
-	private int analisaLinhaPecaTopo(Nodo nodo) {
-		// TODO Auto-generated method stub
-		return 0;
+	private int[] analisaLinhaPecaTopo(Nodo nodo) {
+		int[] sequencia = null;
+		return sequencia;
 	}
 
-	private int analisaColunaPecaTopo(Nodo nodo) {
-		// TODO Auto-generated method stub
-		return 0;
+	private int[] analisaColunaPecaTopo(Nodo nodo) {
+		int[] sequencia = null;
+		return sequencia;
 	}
 }
--- src/trabalho2si/Main.java
+++ src/trabalho2si/Main.java
@@ -8,6 +8,24 @@ public class Main {
 		Nodo nodoVazio = new Nodo(0, matrizVazia, Peca.EMPTY, 0, 0, false, 0, 0);
 		nodoVazio.adicionarPeca(3, Peca.PLAYER_1);
 		nodoVazio.imprimirNodo();
+		Integer vencedor = null;
+		Integer jogadorDaVez = null;
+		jogadorDaVez = perguntarJogadorDaVez();
+		int maiorSequencia = 0;
+		while(vencedor == null){
+			//Jogada
+			nodoVazio.adicionarPeca(3, Peca.PLAYER_1);
+			if(maiorSequencia == 4){
+				vencedor = jogadorDaVez;
+			}
+		}
+	}
+
+	private static Integer perguntarJogadorDaVez() {
+		if(jogador){
+			return 1;
+		}
+		return 2;
 	}
 
 	private static void iniciarMatriz(int[][] matrizVazia) {
