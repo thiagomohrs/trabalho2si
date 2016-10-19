64d3c42b1f68c13580ee404ed6d20048905bae3c
Thiago Mohr <thiagomohrs@gmail.com>
2016-10-19 20:25:21 -0200

    Mudanças raphael

--- src/trabalho2si/Heuristica.java
+++ src/trabalho2si/Heuristica.java
@@ -92,18 +92,55 @@ public class Heuristica {
 		return quantidadeSequencia;
 	}
 
-	private int[] analisaDiagDirPecaTopo(Nodo nodo) {
-		int[] sequencia = null;
-		return sequencia;
+	private int analisaDiagDirPecaTopo(int[] ultimaPosicao, Nodo nodo, int jogador) {
+		int quantidadeSequencia = 1;
+		for (int linha = ultimaPosicao[0]; linha <= 0; linha--) {
+			for (int coluna = ultimaPosicao[1]; coluna <= 7; coluna++) {
+				if(linha >= 0 && linha<= 6 && coluna >= 0 && coluna <= 7){
+					if(nodo.estado[linha][coluna] == jogador){
+						quantidadeSequencia++;
+					} else{
+						return quantidadeSequencia;
+					}
+				}
+			}
+		}
+		return quantidadeSequencia;
 	}
 
-	private int[] analisaLinhaPecaTopo(Nodo nodo) {
-		int[] sequencia = null;
-		return sequencia;
+	private int analisaLinhaPecaTopo(int[] ultimaPosicao, Nodo nodo, int jogador) {
+		int quantidadeSequencia = 1;
+		for (int colunaEsq = ultimaPosicao[1]; colunaEsq <= 0; colunaEsq--) {
+			if(colunaEsq >= 0 && colunaEsq<= 7){
+				if(nodo.estado[colunaEsq][ultimaPosicao[1]] == jogador){
+					quantidadeSequencia++;
+				} else{
+					for (int colunaDir = ultimaPosicao[1]; colunaDir <= 7; colunaDir++) {
+						if(colunaDir >= 0 && colunaDir<= 7){
+							if(nodo.estado[colunaDir][ultimaPosicao[1]] == jogador){
+								quantidadeSequencia++;
+							} 
+						} else{
+							return quantidadeSequencia;
+						}
+					}
+				}
+			}
+		}
+		return quantidadeSequencia;
 	}
 
-	private int[] analisaColunaPecaTopo(Nodo nodo) {
-		int[] sequencia = null;
-		return sequencia;
+	private int analisaColunaPecaTopo(int[] ultimaPosicao, Nodo nodo, int jogador) {
+		int quantidadeSequencia = 1;
+		for (int linha = ultimaPosicao[0]; linha <= 0; linha--) {
+			if(linha >= 0 && linha<= 6){
+				if(nodo.estado[linha][ultimaPosicao[1]] == jogador){
+					quantidadeSequencia++;
+				} else{
+					return quantidadeSequencia;
+				}
+			}
+		}
+		return quantidadeSequencia;
 	}
 }
